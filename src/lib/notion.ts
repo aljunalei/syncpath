176b7d20a63950d002fbea0ebaef948bdc31235f
Aljunalei Alfonso <154396730+Junalei@users.noreply.github.com>
2025-12-31 13:17:21 +0800

    chore: installed tailwind animate dependecy

--- src/lib/notion.ts
+++ src/lib/notion.ts
@@ -6,7 +6,7 @@ export const notion = new Client({
 
 export const databaseId = process.env.NOTION_DATABASE_ID!;
 
-export async function getTask () {
+export async function getTasks () {
     try
     {
     const response = await notion.dataSources.query({
@@ -15,6 +15,8 @@ export async function getTask () {
     
     console.log("Success! Retrieved tasks from Notion database.");
     console.log("Task found:", response);
+
+    return response.results;
 
     } catch (error) {
         console.error("Error retrieving tasks from Notion database:", error);
